58ada8fbc5b51916d37722344684112e90d87105
varvaraperesvet <varvaraperesvet@gmail.com>
2019-07-15 11:54:30 +0300

    an example of code with Geared

---
+++ WithGeared/WithGeared/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WithGeared
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IDisposable
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Dispose()
+        {
+            var vm = (MainWindowVM)DataContext;
+            for (var index = 0; index < vm.Series.Count; index++)
+            {
+                var series = vm.Series[index];
+                var disposable = series.Values as IDisposable;
+                if (disposable != null)
+                {
+                    disposable.Dispose();
+                }
+            }
+
+            vm.Series = null;
+        }
+    }
+}
---
+++ WithGeared/WithGeared/MainWindowVM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Geared;
+
+namespace WithGeared
+{
+    class MainWindowVM
+    {
+        public MainWindowVM()
+        {
+            Series = new SeriesCollection();
+            var r = new Random();
+
+            for (var i = 0; i < 30; i++) // 30 series
+            {
+                var trend = 0d;
+                var values = new double[10000];
+
+                for (var j = 0; j < 10000; j++) // 10k points each
+                {
+                    trend += (r.NextDouble() < .8 ? 1 : -1) * r.Next(0, 10);
+                    values[j] = trend;
+                }
+
+                var series = new GLineSeries
+                {
+                    Values = values.AsGearedValues().WithQuality(Quality.Low),
+                    Fill = Brushes.Transparent,
+                    StrokeThickness = .5,
+                    PointGeometry = null //use a null geometry when you have many series
+                };
+                Series.Add(series);
+            }
+        }
+
+        public SeriesCollection Series { get; set; }
+    }
+}
