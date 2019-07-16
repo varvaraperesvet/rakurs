f51c06aca39aeb1a2763ab8d2171ff961305402f
irina201718 <bi221@yandex.ru>
2019-07-16 08:43:05 +0300

    each marker is located where it is supposed to be. also they are bold and beautiful!

--- ZoneMarkers/ZoneMarkers/testConverter.cs
+++ ZoneMarkers/ZoneMarkers/testConverter.cs
@@ -1,11 +1,9 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 
@@ -15,57 +13,34 @@ namespace ZoneMarkers
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            int i = 0;
-            double iks = 0;
-            double igrik = 0;
-            List <double> arrX = new List<double>();
-            List <double> arrY = new List<double>();
-
             SeriesCollection series = (SeriesCollection)value;
             if (series != null)
             {
+                VisualElementsCollection result = new VisualElementsCollection();
+                LineSeries prevLine = null;
                 foreach (var serie in series)
                 {
                     LineSeries line = (LineSeries)serie;
                     {
                         if (series != null)
                         {
-                            foreach (var point in line.ChartPoints)
+                            double iks = 0.25 * line.ChartPoints.Max(p => p.X);
+                            ChartPoint игрик = line.ChartPoints.First(p => p.X > iks);
+                            double gaga = игрик.Y*0.5;
+                            if (prevLine != null)
                             {
-                                arrX.Add(point.X);
-                                arrY.Add(point.Y);
-                            }
+                                ChartPoint irgikOLD = prevLine.ChartPoints.OrderBy(p => p.X - iks).First();
+                                gaga = (игрик.Y - irgikOLD.Y)*0.9+irgikOLD.Y;
+                             }
+                            result.Add(new VisualElement() { X = iks, Y = gaga, UIElement = new TextBlock() { Text = serie.Title, FontWeight = FontWeights.Bold, FontSize = 15}});
                         }
-
                     }
+                    prevLine = line;
                 }
-
-
-                double distrikt = arrX.Max()-arrX.Min(); // задаём местоположение надписи по Х
-                iks = distrikt * 0.25;
-
-                i = 0;
-                while (iks> arrX[i]) // борщ соответствующий igrik
-                {
-                    if (iks < arrX[i+1])
-                    {
-                        if ((iks - arrX[i]) > (arrX[i + 1]-iks))
-                            igrik = arrY[i + 1] * 0.5;
-                        else
-                        {
-                            igrik = arrY[i] * 0.5;
-                        }
-
-                        break;
-                    }
-                    i = i + 1;
-                }
-
-
-                return new VisualElementsCollection() { new VisualElement() { X = iks, Y = igrik, UIElement = new TextBlock() { Text = "A" } } };
+                return result;
             }
             else
-                return new VisualElementsCollection() { new VisualElement() { X = iks, Y = igrik, UIElement = new TextBlock() { Text = "ффух" } } };
+                return new VisualElementsCollection() { new VisualElement() };
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
